7126967167ad1ab5a5f234575691958084e5fb51
Eli Morris <elimorris@Elis-MBP.lan>
2023-04-11 21:48:20 -0700

    adding API POST req, adding notesData.js file

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 const express = require("express");
-
+//Express app
 var app = express();
-var PORT = process.env.PORT || 3000;
+//var PORT = process.env.PORT || 3000;      <-- File Issue
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.static('db'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes);
-});
-
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-
-    return newNote;
-}
+require("./routes/apiRoutes")(app);
+requrie("./routes/apiRoutes")(app);
 
 app.listen(PORT, () => {
     console.log('API server now on port ${PORT}!');
